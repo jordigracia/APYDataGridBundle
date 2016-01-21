7f30f41467511fccf39b1ee0ffae98ab9bfc5a0b
Jordi <j.gracia83@gmail.com>
2016-01-21 17:24:28 +0100

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -38,11 +38,7 @@ class Configuration implements ConfigurationInterface
                 ->scalarNode('actions_columns_size')->defaultValue(-1)->end()
                 ->scalarNode('actions_columns_title')->defaultValue('Actions')->end()
                 ->scalarNode('actions_columns_separator')->defaultValue('<br />')->end() // deprecated
-                ->arrayNode('jordigracia')
-                    ->children()
-                         ->scalarNode('protocol')->defaultValue('http')->end()
-                    ->end()         
-                ->end()
+               
                 ->arrayNode('pagerfanta')
                     ->addDefaultsIfNotSet()
                     ->children()
